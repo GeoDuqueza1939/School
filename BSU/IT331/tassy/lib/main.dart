628467034af6496addbf967244e39eb4fc6301e1
Geovani Duqueza <geovaniduqueza1939@yahoo.com>
2025-07-26 07:09:57 +0800

    Updated final project

--- BSU/IT331/tassy/lib/main.dart
+++ BSU/IT331/tassy/lib/main.dart
@@ -1,6 +1,6 @@
 //==============================================================
 // Program Name:  TASSY: The Task Assistant System
-// Description:   A simple task list app
+// Description:   A simple task list application
 // Author:        Geovani P. Duqueza
 //==============================================================
 
@@ -10,145 +10,114 @@ void main() {
   runApp(TassyApp());
 }
 
-// ignore: must_be_immutable
 class TassyApp extends StatelessWidget {
-  Map<String, Object> themes = {
+  const TassyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TassyMain();
+  }
+}
+
+// ignore: must_be_immutable
+class TassyMain extends StatefulWidget {
+  final String title = "TASSY"; // Appbar title
+  final Map<String, Object> themes = {
       "theme": ThemeData(
-        /*
-        This is the theme of your application.
-        
-        TRY THIS: Try running your application with "flutter run". You'll see
-        the application has a purple toolbar. Then, without quitting the app,
-        try changing the seedColor in the colorScheme below to Colors.green
-        and then invoke "hot reload" (save your changes or press the "hot
-        reload" button in a Flutter-supported IDE, or press "r" if you used
-        the command line to start the app).
-        
-        Notice that the counter didn't reset back to zero; the application
-        state is not lost during the reload. To reset the state, use hot
-        restart instead.
-        
-        This works for code too, not just values: Most code changes can be
-        tested with just a hot reload.
-        */
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue, brightness: Brightness.light,),
       ),
       "darkTheme": ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.grey, brightness: Brightness.dark,),
       ),
-      "themeMode": ThemeMode.dark,
+      "themeMode": ThemeMode.light,
   };
 
-  TassyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'TASSY: The Task Assistant System',
-      home: const TassyMain(title: 'TASSY'),
-      theme: themes["theme"] as ThemeData,
-      darkTheme: themes["darkTheme"] as ThemeData,
-      themeMode: themes["themeMode"] as ThemeMode,
-    );
-  }
-}
-
-class TassyMain extends StatefulWidget {
-  const TassyMain({super.key, required this.title});
-  /*
-  This widget is the home page of your application. It is stateful, meaning
-  that it has a State object (defined below) that contains fields that affect
-  how it looks.
-
-  This class is the configuration for the state. It holds the values (in this
-  case the title) provided by the parent (in this case the App widget) and
-  used by the build method of the State. Fields in a Widget subclass are
-  always marked "final".
-  */
-
-  final String title;
+  TassyMain({super.key});
 
   @override
   State<TassyMain> createState() => _TassyMainState();
 }
 
 class _TassyMainState extends State<TassyMain> {
-  int _counter = 0;
+  int _counter = 0; // FROM DEMO! PLEASE REMOVE/EDIT!
+
+  _TassyMainState();
 
   @override
   Widget build(BuildContext context) {
-    /*
-    This method is rerun every time setState is called, for instance as done
-    by the _incrementCounter method above.
-    
-    The Flutter framework has been optimized to make rerunning build methods
-    fast, so that you can just rebuild anything that needs updating rather
-    than having to individually change instances of widgets.
-    */
-    return Scaffold(
-      appBar: AppBar(
-        /* 
-        TRY THIS: Try changing the color here to a specific color (to
-        Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        change color while the other colors stay the same.
-        */
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        /* 
-        Here we take the value from the MyHomePage object that was created by
-        the App.build method, and use it to set our appbar title.
-        */ 
-        title: Text(widget.title),
-      ),
-      body: Center(
-        /* 
-        Center is a layout widget. It takes a single child and positions it
-        in the middle of the parent.
-        */ 
-        child: Column(
-          /* 
-          Column is also a layout widget. It takes a list of children and
-          arranges them vertically. By default, it sizes itself to fit its
-          children horizontally, and tries to be as tall as its parent.
-          
-          Column has various properties to control how it sizes itself and
-          how it positions its children. Here we use mainAxisAlignment to
-          center the children vertically; the main axis here is the vertical
-          axis because Columns are vertical (the cross axis would be
-          horizontal).
-          
-          TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          action in the IDE, or press "p" in the console), to see the
-          wireframe for each widget.
-          */ 
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return MaterialApp(
+      title: 'TASSY: The Task Assistant System', // application title bar/browser title
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text('You have pushed the button this many times:'), // FROM DEMO! PLEASE REMOVE/EDIT!
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headlineMedium, // FROM DEMO! PLEASE REMOVE/EDIT!
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _incrementCounter,
+          tooltip: 'Increment',
+          child: const Icon(Icons.add),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      theme: widget.themes["theme"] as ThemeData,
+      darkTheme: widget.themes["darkTheme"] as ThemeData,
+      themeMode: widget.themes["themeMode"] as ThemeMode,
     );
   }
 
   void _incrementCounter() {
     setState(() {
-      /*
-      This call to setState tells the Flutter framework that something has
-      changed in this State, which causes it to rerun the build method below
-      so that the display can reflect the updated values. If we changed
-      _counter without calling setState(), then the build method would not be
-      called again, and so nothing would appear to happen.
-      */
-      _counter++;
+      _counter++; // FROM DEMO! PLEASE REMOVE/EDIT!
+
+      // widget.themes["themeMode"] = (widget.themes["themeMode"] as ThemeMode == ThemeMode.dark ? ThemeMode.light : ThemeMode.dark);
     });
+  }
+}
+
+enum TimeUnit {
+  second, minute, hour, day, week, month, year, decade, century, millenium
+}
+
+class TassyTask {
+  String? taskName;
+  DateTime? schedule;
+  double duration;
+  TimeUnit durationUnit;
+  List<DateTime>? reminders;
+
+  TassyTask({required this.taskName, this.schedule, this.duration = 0, this.durationUnit = TimeUnit.hour, this.reminders});
+}
+
+class TassyReminder {
+  DateTime? reminderSchedule;
+  double _snoozeDuration = 0; // zero means disabled snooze
+  TimeUnit _snoozeUnit = TimeUnit.minute;
+
+  TassyReminder({required this.reminderSchedule, double snoozeDuration = 0, TimeUnit snoozeUnit = TimeUnit.minute}) {
+    setSnooze(snoozeDuration, unit: snoozeUnit);
+  }
+
+  void setSnooze(double duration, {TimeUnit unit = TimeUnit.minute}) {
+    _snoozeDuration = duration;
+    _snoozeUnit = unit;
+  }
+
+  double get snoozeDuration {
+    return _snoozeDuration;
+  }
+
+  TimeUnit get snoozeUnit {
+    return _snoozeUnit;
   }
 }
