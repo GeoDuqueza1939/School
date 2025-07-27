6d457a081fa10be3ae84a9412db580e165cf3683
Geovani Duqueza <geovaniduqueza1939@yahoo.com>
2025-07-28 04:32:16 +0800

    Added app exit functionality and refactored code * Added 3 imports * Created MsgBox mixin for displaying simple dialogs (TO DO other dialog methods) * Added onTap to Exit item in drawer * Added generateDrawer function * Converted generatePages into a function

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -5,8 +5,11 @@
 //==============================================================
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:string_to_color/string_to_color.dart';
 import 'package:window_size/window_size.dart';
+import 'dart:io';
 
 void main() {
   final String appTitle = "TASSY: The Task Assistant System";
@@ -101,13 +104,13 @@ class TassyAppState extends State<TassyApp> {
   }
 }
 
-class TassyMainState extends State<TassyMain> {
+class TassyMainState extends State<TassyMain> with MsgBox {
   List<Widget> pages = [];
   // ignore: prefer_final_fields
   int _currentPage = 0;
 
   TassyMainState() {
-    generatePages();
+    pages = generatePages();
   }
 
   @override
@@ -118,60 +121,7 @@ class TassyMainState extends State<TassyMain> {
         backgroundColor: Theme.of(context).colorScheme.primary,
         foregroundColor: Theme.of(context).colorScheme.onPrimary,
       ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Theme.of(context).colorScheme.primary
-              ),
-              child: Center(
-                child: Text("TASSY Menu"),
-              ),
-            ),
-            ListTile(
-              leading: Icon(Icons.home_rounded),
-              title: Text("Home"),
-              onTap: () {
-                viewPage(0);
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.add_task_rounded),
-              title: Text("New Task"),
-              onTap: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => TassyTaskEditor(EditorMode.add)));
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.task_rounded),
-              title: Text("View Tasks"),
-              onTap: () {
-                viewPage(1);
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.account_circle),
-              title: Text("User Profile"),
-              onTap: () {
-                viewPage(2);
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.settings_rounded),
-              title: Text("Settings"),
-              onTap: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => TassySettings()));
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.exit_to_app_rounded),
-              title: Text("Exit"),
-            ),
-          ],
-        ),
-      ),
+      drawer: generateDrawer(context),
       body: Container(
         // decoration: BoxDecoration(color: Theme.of(context).colorScheme.secondaryContainer),
         // margin: EdgeInsets.all(0),
@@ -183,7 +133,7 @@ class TassyMainState extends State<TassyMain> {
           ),
         ),
       ),
-      floatingActionButton: FloatingActionButton.extended(
+      floatingActionButton: FloatingActionButton.extended( // FOR TESTING ONLY
         label: Text("Theme: ${(context.findAncestorStateOfType<TassyAppState>() as TassyAppState).selectedThemeName}"),
         onPressed: () {
           TassyAppState appState = context.findAncestorStateOfType<TassyAppState>() as TassyAppState;
@@ -206,8 +156,66 @@ class TassyMainState extends State<TassyMain> {
     );
   }
 
-  void generatePages() {
-    pages = [
+  Widget generateDrawer(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.primary
+            ),
+            child: Center(
+              child: Text("TASSY Menu"),
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.home_rounded),
+            title: Text("Home"),
+            onTap: () {
+              viewPage(0);
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.add_task_rounded),
+            title: Text("New Task"),
+            onTap: () {
+              viewRoute((context) => TassyTaskEditor(EditorMode.add));
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.task_rounded),
+            title: Text("View Tasks"),
+            onTap: () {
+              viewPage(1);
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.account_circle),
+            title: Text("User Profile"),
+            onTap: () {
+              viewPage(2);
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.settings_rounded),
+            title: Text("Settings"),
+            onTap: () {
+              viewRoute((context) => TassySettings());
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.exit_to_app_rounded),
+            title: Text("Exit"),
+            onTap: exitApp,
+          ),
+        ],
+      ),
+    );
+  }
+
+  List<Widget> generatePages() {
+    return <Widget>[
       Container(
         child: Text("Home"),
       ),
@@ -225,6 +233,41 @@ class TassyMainState extends State<TassyMain> {
     setState(() {
       Navigator.pop(context);
       _currentPage = num;
+    });
+  }
+
+  void viewRoute(WidgetBuilder f) {
+    Navigator.pop(context);
+    Navigator.push(context, MaterialPageRoute(builder: f));
+  }
+
+  void exitApp() async {
+    final dev = DeviceInfoPlugin();
+    final devInfo = await dev.deviceInfo;
+    final allInfo = devInfo.data;
+
+    setState(() {
+      String devType = (allInfo.containsKey('servicePackMajor') ? 'Windows'
+        : (allInfo.containsKey('prettyName') ? 'Linux'
+        : (allInfo.containsKey('osRelease') ? 'MacOS'
+        : (allInfo.containsKey('utsname') ? 'IOS'
+        : (allInfo.containsKey('systemFeatures') ? 'Android'
+        : (allInfo.containsKey('browserName') ? 'Web'
+        : 'unknown'))))));
+          
+      switch (devType) {
+        case 'Windows': case 'Linux': case 'MacOS':
+          exit(0); // REPLACE WITH A SAFER EXIT OPTION ONCE AVAILABLE!!!!
+          // ignore: dead_code
+          break;
+        case 'IOS': case 'Android':
+          SystemNavigator.pop();
+          break;
+        case 'Web': case 'unknown':
+          Navigator.pop(context);
+          _simpleMsg(context, 'To close this app, please close the browser tab or window manually.', title: (context.findAncestorWidgetOfExactType<TassyApp>() as TassyApp).title);
+          break;
+      }
     });
   }
 }
@@ -254,6 +297,28 @@ class TassyTaskEditorState extends State<TassyTaskEditor> {
           ),
         ],
       ),
+    );
+  }
+}
+
+mixin MsgBox {
+  Future _simpleMsg(BuildContext context, String msg, {String title = ""}) {
+    return showDialog(
+      context: context, 
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title),
+          content: Text(msg),
+          actions: [
+            TextButton(
+              child: const Text("OK"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
