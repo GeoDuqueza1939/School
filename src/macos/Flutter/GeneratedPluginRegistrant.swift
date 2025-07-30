bb3e951e551edaa8b68fd442d2e85bb14632c23f
Geovani Duqueza <geovaniduqueza1939@yahoo.com>
2025-07-30 20:51:55 +0800

    Commit changes to dependencies

--- src/macos/Flutter/GeneratedPluginRegistrant.swift
+++ src/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,13 @@ import FlutterMacOS
 import Foundation
 
 import device_info_plus
+import path_provider_foundation
+import shared_preferences_foundation
 import window_size
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   WindowSizePlugin.register(with: registry.registrar(forPlugin: "WindowSizePlugin"))
 }
